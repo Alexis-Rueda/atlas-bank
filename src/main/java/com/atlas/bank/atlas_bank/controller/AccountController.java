17cae6c08d0c6f52bd4a4634aa5963003c3de092
alexis.rueda <alexisrueda34@gmail.com>
2026-08-19 23:19:18 -0600

    Add transaction management with transfer and transaction retrieval functionality

--- src/main/java/com/atlas/bank/atlas_bank/controller/AccountController.java
+++ src/main/java/com/atlas/bank/atlas_bank/controller/AccountController.java
@@ -1,12 +1,14 @@
 package com.atlas.bank.atlas_bank.controller;
 
 import com.atlas.bank.atlas_bank.model.Account;
+import com.atlas.bank.atlas_bank.model.Transaction;
 import com.atlas.bank.atlas_bank.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -29,5 +31,18 @@ public class AccountController {
     @GetMapping("/{id}")
     public ResponseEntity<Account> findById(@PathVariable Long id){
         return ResponseEntity.ok(accountService.findById(id));
+    }
+
+
+    @PostMapping("/transfer")
+    public ResponseEntity<Transaction> transfer(@RequestParam Long fromId,
+                                                @RequestParam Long toId,
+                                                @RequestParam BigDecimal amount){
+        return ResponseEntity.ok(accountService.transfer(fromId, toId, amount));
+    }
+
+    @GetMapping("/{id}/transactions")
+    public ResponseEntity<List<Transaction>> getTransactions(@PathVariable Long id){
+        return ResponseEntity.ok(accountService.getTransactions(id));
     }
 }
