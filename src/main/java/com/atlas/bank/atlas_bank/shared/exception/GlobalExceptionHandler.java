30fc78d46ce8445b428a8deaf3ddaadb6927b50c
alexis.rueda <alexisrueda34@gmail.com>
2026-08-22 00:02:55 -0600

    feat: enhance validation for account creation and transfer requests

--- src/main/java/com/atlas/bank/atlas_bank/shared/exception/GlobalExceptionHandler.java
+++ src/main/java/com/atlas/bank/atlas_bank/shared/exception/GlobalExceptionHandler.java
@@ -6,8 +6,12 @@ import com.atlas.bank.atlas_bank.transaction.exception.InsufficientFundsExceptio
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -45,6 +49,23 @@ public class GlobalExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR, "Error interno del servidor"
         );
         problem.setTitle("Error interno");
+        return problem;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidation(MethodArgumentNotValidException ex) {
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problem.setTitle("Error de validación");
+
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach( error -> errors.add(error.getField() + ": " + error.getDefaultMessage()));
+
+        ex.getBindingResult().getGlobalErrors()
+                .forEach(error -> errors.add(error.getDefaultMessage()));
+
+        problem.setProperty("errors", errors);
+
         return problem;
     }
 
