23b8a7a5a419fda2a8cafd2df1f0e41a54f1495f
alexis.rueda <alexisrueda34@gmail.com>
2026-08-28 00:11:02 -0600

    Proxy

--- src/main/java/com/atlas/bank/atlas_bank/account/service/AccountService.java
+++ src/main/java/com/atlas/bank/atlas_bank/account/service/AccountService.java
@@ -4,6 +4,8 @@ import com.atlas.bank.atlas_bank.account.exception.AccountNotFoundException;
 import com.atlas.bank.atlas_bank.account.model.Account;
 import com.atlas.bank.atlas_bank.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AccountService implements IAccountService {
     private final AccountRepository accountRepository;
 
@@ -28,6 +31,7 @@ public class AccountService implements IAccountService {
 
     @Override
     @Transactional(readOnly = true)
+    @Cacheable(value = "accounts", key = "#id")
     public Account findById(Long id){
         return accountRepository.findById(id).orElseThrow(
                 () -> new AccountNotFoundException(id)
--- src/main/java/com/atlas/bank/atlas_bank/account/service/AuditableAccountService.java
+++ src/main/java/com/atlas/bank/atlas_bank/account/service/AuditableAccountService.java
@@ -1,6 +1,7 @@
 package com.atlas.bank.atlas_bank.account.service;
 
 import com.atlas.bank.atlas_bank.account.model.Account;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
@@ -17,6 +18,12 @@ public class AuditableAccountService implements IAccountService{
 
     public AuditableAccountService(@Qualifier("accountService") IAccountService delegate) {
         this.delegate = delegate;
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        log.info("Clase real del accountService: {}", delegate.getClass().getName());
     }
 
     @Override
