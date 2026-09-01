9f30dce6fda1d646e0e4797022ddcd04b810ec9b
alexis.rueda <alexisrueda34@gmail.com>
2026-08-31 23:39:24 -0600

    add ports out

---
+++ src/main/java/com/atlas/bank/atlas_bank/application/port/out/FraudCheckPort.java
@@ -0,0 +1,10 @@
+package com.atlas.bank.atlas_bank.application.port.out;
+
+
+import com.atlas.bank.atlas_bank.transaction.service.fraud.FraudCheckResult;
+
+import java.math.BigDecimal;
+
+public interface FraudCheckPort {
+    FraudCheckResult check(Long accountId, BigDecimal amount);
+}
