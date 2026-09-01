2463b694627ebaa992bdefa9f418d4390485abb2
alexis.rueda <alexisrueda34@gmail.com>
2026-08-31 23:51:08 -0600

    JPA como adaptador

--- src/main/java/com/atlas/bank/atlas_bank/transaction/service/transfer/TransferService.java
+++ src/main/java/com/atlas/bank/atlas_bank/transaction/service/transfer/TransferService.java
@@ -2,8 +2,8 @@ package com.atlas.bank.atlas_bank.transaction.service.transfer;
 
 import com.atlas.bank.atlas_bank.account.exception.AccountNotFoundException;
 import com.atlas.bank.atlas_bank.account.model.Account;
-import com.atlas.bank.atlas_bank.account.repository.DomainAccountRepository;
 import com.atlas.bank.atlas_bank.application.port.in.TransferMoneyUseCase;
+import com.atlas.bank.atlas_bank.application.port.out.AccountRepositoryPort;
 import com.atlas.bank.atlas_bank.transaction.model.Transaction;
 import com.atlas.bank.atlas_bank.transaction.repository.TransactionRepository;
 import com.atlas.bank.atlas_bank.transaction.service.domain.TransferDomainService;
@@ -20,14 +20,14 @@ import java.util.List;
 public class TransferService extends TransactionProcessor<TransferContext>
         implements ITransferService, TransferMoneyUseCase {
 
-    private final DomainAccountRepository accountRepository;
+    private final AccountRepositoryPort accountRepository;
     private final List<FeeCalculator> feeCalculators;
     private final List<TransferValidator> validators;
     private final TransferDomainService transferDomainService;
 
     public TransferService(
             TransactionRepository transactionRepository,
-            DomainAccountRepository accountRepository,
+            AccountRepositoryPort accountRepository,
             List<FeeCalculator> feeCalculators,
             List<TransferValidator> validators,
             TransferDomainService transferDomainService
